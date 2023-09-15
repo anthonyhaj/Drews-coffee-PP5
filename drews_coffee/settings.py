76cc13dc35fc96fbd2da66e984a98e6b9ea25868
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-15 17:28:41 +0000

    fix staticfiles dir path

--- drews_coffee/settings.py
+++ drews_coffee/settings.py
@@ -159,7 +159,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
