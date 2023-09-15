58af85cf433628b7ad0f468afbdcb6e65be7c04c
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-15 09:13:55 +0000

    add Procfile and deploy settings to settings.py

--- drews_coffee/settings.py
+++ drews_coffee/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-anthonyhaj-drewscoffeep-l527csiiolf.ws-eu104.gitpod.io',]
+ALLOWED_HOSTS = ['8000-anthonyhaj-drewscoffeep-l527csiiolf.ws-eu104.gitpod.io', 'https://drews-coffee-pp5-3d45d25f45a2.herokuapp.com/',]
 
 
 # Application definition
@@ -109,10 +109,17 @@ WSGI_APPLICATION = 'drews_coffee.wsgi.application'
     }
 } """
 
-DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
