75081aec5757bdcdfc8d53acfbf94d80f6ebef1f
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-15 14:56:30 +0000

    adjust aws settings in settings.py for test

--- drews_coffee/settings.py
+++ drews_coffee/settings.py
@@ -169,7 +169,7 @@ if 'USE_AWS' in os.environ:
 
     AWS_STORAGE_BUCKET_NAME = 'drews-coffee-pp5'
     AWS_S3_REGION_NAME = 'eu-north-1'
-    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
@@ -178,6 +178,7 @@ if 'USE_AWS' in os.environ:
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
+
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
