2ab1662ee2f8c3b7e828b1103fe467ed5565c5ba
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-17 16:16:59 +0000

    add admin.py register models products and categories

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 from .models import Category, Product
-# Register your models here.
+
+from django.contrib import admin
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'sku', 'price', 'category', 'rating')
+    ordering = ('name',)
+    search_fields = ('name', 'sku', 'category__name')
+    list_filter = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name')
+    ordering = ('name',)
+    search_fields = ('name', 'friendly_name')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
         return self.name
 
     def get_friendly_name(self):
-        return self.friendly_name   
+        return self.friendly_name 
 
 
 class Product(models.Model):
