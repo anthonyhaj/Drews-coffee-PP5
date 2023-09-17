9bdb08b8b362e2ce13e6604d62bc1fee14183a97
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-17 15:57:54 +0000

    add friendly name return to models.py for products app

--- products/models.py
+++ products/models.py
@@ -1,8 +1,35 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=50, db_index=True)
     friendly_name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name   
+
+
+class Product(models.Model):
+    sku = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=50, db_index=True)
+    description = models.TextField()
+    has_sizes = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(
+        Category, related_name='products',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        null=True,
+        blank=True
+    )
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
