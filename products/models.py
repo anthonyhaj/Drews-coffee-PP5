e9c01cbf559699147d3ec0cd8d0fd995fbf904ff
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-17 16:58:26 +0000

    add product image model for adding extra images and register in admin.py

--- products/models.py
+++ products/models.py
@@ -33,3 +33,26 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, 
+        related_name='product_images', 
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to='product_images/')
+    
+    def __str__(self):
+        return self.product.name + " Image"
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, 
+        related_name='product_images', 
+        on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to='product_images/')
+    
+    def __str__(self):
+        return self.product.name + " Image"
