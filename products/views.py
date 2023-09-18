6602473b947b2a2959f392b203b19d6acac2da53
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-18 16:09:47 +0000

    redone all_products view and adjust category selector in products.html

--- products/views.py
+++ products/views.py
@@ -1,39 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Product, Category
 from django.db.models import Q
 from django.db.models.functions import Lower
-
+from django.contrib import messages
 
 def all_products(request):
-    queryset = Product.objects.all()
-    query = request.GET.get('q')
+    """ A view to show all products, including sorting and search queries """
 
-    # Search Query
-    if query:
-        queryset = queryset.filter(
-            Q(name__icontains=query) |
-            Q(description__icontains=query)
-            
-        ).order_by(Lower('name'), Lower('description'))
-
-    # Sorting
-    sort_by = request.GET.get('sort_by', 'name_az')
-    sort_options = {
-        'price_low': 'price',
-        'price_high': '-price',
-        'rating_low': 'rating',
-        'rating_high': '-rating',
-        'category_az': Lower('category__name'),
-        'category_za': Lower('category__name').desc(),
-        'name_az': Lower('name'),
-        'name_za': Lower('name').desc()
-    }
-    sort_order = sort_options.get(sort_by, Lower('name'))
-    queryset = queryset.order_by(sort_order)
+    # Fetch initial queryset and initialize variables
+    products = Product.objects.all()
     categories = Category.objects.all()
+    query = None
+    sort_by = None
+    direction = None
+    filtered_categories = None
+
+    # Check for GET parameters
+    if request.GET:
+        # Handle sorting
+        if 'sort_by' in request.GET:
+            sort_by = request.GET['sort_by']
+            sort_options = {
+                'price_low': 'price',
+                'price_high': '-price',
+                'rating_low': 'rating',
+                'rating_high': '-rating',
+                'category_az': Lower('category__name'),
+                'category_za': Lower('category__name').desc(),
+                'name_az': Lower('name'),
+                'name_za': Lower('name').desc()
+            }
+            sort_order = sort_options.get(sort_by, Lower('name'))
+            products = products.order_by(sort_order)
+
+        # Handle category filter
+        if 'category' in request.GET:
+            filtered_categories = request.GET['category'].split(',')
+            products = products.filter(
+                category__name__in=filtered_categories
+            )
+            filtered_categories = Category.objects.filter(
+                name__in=filtered_categories
+            )
+
+        # Handle search queries
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))          
+            queries = Q(
+                name__icontains=query
+            ) | Q(
+                description__icontains=query
+            )
+            products = products.filter(
+                queries
+            ).order_by(Lower('name'), Lower('description'))
+
+    # Prepare context for template
     context = {
-        'products': queryset,
-        'sort_by': sort_by,
+        'products': products,
+        'search_term': query,
         'categories': categories
+        if filtered_categories is None else filtered_categories,
+        'sort_by': sort_by
     }
+
     return render(request, 'products/products.html', context)
