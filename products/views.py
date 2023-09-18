276652e8587abbb0e801cadace97baedf3f805ad
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-18 16:41:26 +0000

    add main sturcture to product_detail.html

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product, Category
 from django.db.models import Q
 from django.db.models.functions import Lower
