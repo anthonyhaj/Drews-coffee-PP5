9b50fcadbc16e2dc92ad6c761b3b4847e0a7109c
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-25 15:55:48 +0000

    adjust checkout view for saving orderlineitem and add more structure to checkout_succes.html

--- checkout/models.py
+++ checkout/models.py
@@ -83,7 +83,7 @@ class Order(models.Model):
         Update grand total each time a line item is added
         """
         self.order_total = self.lineitems.aggregate(
-            Sum('lineitem_total'))['lineitem_total__sum'] or 0
+            Sum('line_item_total'))['line_item_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = (
                 self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -108,7 +108,11 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
-        Order, null=False, blank=False, on_delete=models.CASCADE
+        Order,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='lineitems'
     )
     product = models.ForeignKey(
         Product, null=False, blank=False, on_delete=models.CASCADE
@@ -128,7 +132,7 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.line_item_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
--- checkout/views.py
+++ checkout/views.py
@@ -8,12 +8,15 @@ from django.contrib import messages
 from django.conf import settings
 import stripe
 import json
+import logging
 # Internal
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product, Category
 from bag.contexts import bag_contents
+from bag.templatetags import bag_filters
+
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -21,6 +24,7 @@ from bag.contexts import bag_contents
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+    logger = logging.getLogger(__name__)
 
     if request.method == 'POST':
         bag = request.session.get('bag', {})
@@ -36,43 +40,31 @@ def checkout(request):
             'address2': request.POST['address2'],
             'county': request.POST['county'],
         }
-
         order_form = OrderForm(form_data)
+        
         if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_bag = json.dumps(bag)
-            order.save()
+            order = order_form.save()
             for item_id, item_data in bag.items():
+                print(f"Item data: {item_data}")
                 try:
                     product = Product.objects.get(id=item_id)
-                    if isinstance(item_data, int):
+                    if 'items_by_quantity' in item_data:
+                        quantity = item_data['items_by_quantity']
                         order_line_item = OrderLineItem(
                             order=order,
                             product=product,
-                            quantity=item_data,
+                            quantity=quantity,
                         )
                         order_line_item.save()
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                product=product,
-                                quantity=quantity,
-                                product_size=size,
-                            )
-                            order_line_item.save()
+                        
                 except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasn't \
-                             found in our database."
+                        "One of the products in your bag wasn't found in our database. "
                         "Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
-            # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
@@ -93,6 +85,8 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
+        order_form = OrderForm()
+
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
@@ -105,4 +99,23 @@ def checkout(request):
     }
 
     return render(request, template, context)
-    
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
