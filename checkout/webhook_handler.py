d97ae65b6863f9225a2c90445dec414064a1d0e7
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-25 18:17:21 +0000

    add webhook function to webhooks.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -16,3 +16,20 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the webhook from Stripe for payment_intent.succeeded
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created\
+                 order in webhook',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the webhook from Stripe for payment_intent.payment_failed
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
