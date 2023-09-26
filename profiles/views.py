00d73ad89ad5b8ad79b0ac85866715e4fb4263f0
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-26 13:39:04 +0000

    add finishing touches to profile.html, add order_history view, adjust country_field.js

--- profiles/views.py
+++ profiles/views.py
@@ -16,6 +16,13 @@ from products.models import Product, Category
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
@@ -23,6 +30,24 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is an old confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
