2dc083aaf0a1d7e8b2e19ded28ebb1048786b711
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-26 09:33:50 +0000

    add styles for select elements

--- checkout/admin.py
+++ checkout/admin.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('line_item_total',)
