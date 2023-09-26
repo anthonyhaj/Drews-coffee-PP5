984b09467dc1b7c7eabce82b8c5e0c92064ca583
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-26 10:20:40 +0000

    add profile url path, update templates in allauth for futrue modifying

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd Party
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+# Internal
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from .models import UserProfile
+from checkout.models import Order
+from products.models import Product, Category
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 def profile(request):
     """ Display the user's profile. """
