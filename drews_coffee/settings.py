24d88b9a8d6a571927afb16bdc5ce61a71cc7d2c
Anthony Haj Ibrahim <anthony.hajibrahim@gmail.com>
2023-09-27 12:16:48 +0000

    deploy test fix 1

--- drews_coffee/settings.py
+++ drews_coffee/settings.py
@@ -199,9 +199,9 @@ if 'USE_AWS' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
-    # Override static and media URLs in production
+    """ # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/' """
 
     # Cache Control
     AWS_S3_OBJECT_PARAMETERS = {
